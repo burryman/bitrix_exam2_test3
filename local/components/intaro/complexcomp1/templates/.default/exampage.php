5663603b1e8c3f634ad8c483cbb6c4831e8c6a3c
Ilya Krasnitsky <burrmanapps@gmail.com>
2018-02-02 10:56:14 +0300

    ex 2-25

---
+++ local/components/intaro/complexcomp1/templates/.default/exampage.php
@@ -0,0 +1,5 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?
+    echo "<b>PARAM1: </b>" . $arResult['VARIABLES']['PARAM1'] . "<br>";
+    echo "<b>PARAM2: </b>" . $arResult['VARIABLES']['PARAM2'] . "<br>";
+?>
